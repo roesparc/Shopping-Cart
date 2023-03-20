9bf19103dad6de73f5024358a89e9ef9049e1fc5
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-03-20 14:12:21 -0600

    Add styled components compatibility with jest

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import Footer from "../components/Footer";
+import "jest-styled-components";
 
 describe("Footer component", () => {
   test("Renders correct static text and links", () => {
