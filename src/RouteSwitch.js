00d66d95eed99c2cba004470575c119e5451baef
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-03-20 14:10:15 -0600

    Add layout to RouteSwitch component

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,14 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { CartProvider } from "./contexts/CartContext";
+import StyledRouteSwitch from "./styles/StyledRouteSwitch";
 
-const RouteSwitch = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Layout></Layout>
-      </BrowserRouter>
-    </>
-  );
-};
+const RouteSwitch = () => (
+  <StyledRouteSwitch>
+    <BrowserRouter>
+      <CartProvider>
+        <Header />
+        <Main />
+      </CartProvider>
+
+      <Footer />
+    </BrowserRouter>
+  </StyledRouteSwitch>
+);
 
 export default RouteSwitch;
