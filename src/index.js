246a34022274a43ac5fb0870827d3f8ba13baacf
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-03-27 10:50:05 -0600

    Add theme provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import RouteSwitch from "./RouteSwitch";
 import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <RouteSwitch />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouteSwitch />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/Theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    main: "#212a2f",
+    mainTwo: "#c75959",
+    mainThree: "#00163b",
+    white: "#ffffff",
+  },
+  shadows: {
+    logoShadow: "0 0 2px",
+    headerShadow: "0 0 4px #bdbdbd",
+    mainBoxShadow: " 0px 1px 2px 1px #212a2f33",
+  },
+  fonts: {
+    logo: "Allura, sans-serif",
+    main: "Open Sans, sans-serif",
+  },
+};
+
+export default theme;
